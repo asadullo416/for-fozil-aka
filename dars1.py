d6172fcf4f39d3d0cbfefae29e2742dc629002f5
asadullo416 <kuchkarovasadullo2@gmail.com>
2023-01-11 20:16:07 +0500

    for-fozil-aka

---
+++ dars1.py
@@ -0,0 +1,30 @@
+massiv = ['olma', 'banan', 'mandarin', 'nok', 'anjir']
+
+# massiv.sort()
+# print(massiv)
+# print('massiv elementlarining soni', len(massiv))
+
+
+a = 100
+b = 2
+d = a/b
+j = a//b
+# print(d)
+# print(j)
+
+
+radius = 30
+PI = 3.14159
+diametr = 2*30
+# print('aylananing uzunligi', PI*diametr, 'ga teng')
+
+
+butun_son = 10
+onlik_son = 5.5
+string = '12'
+# print(float(butun_son)) 
+# print(int(onlik_son))
+# print(int(string))
+
+
+
